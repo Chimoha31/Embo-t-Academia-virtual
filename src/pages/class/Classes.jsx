9c37430134787d70b29549936ebad12ae2f3a0dd
Chimoha31 <mokochii1108@gmail.com>
2022-10-06 17:21:19 -0700

    Created 70% Classes

--- src/pages/class/Classes.jsx
+++ src/pages/class/Classes.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import "./Classes.scss";
+import AllClasses from "../../data/AllClasses";
 
 const Classes = () => {
   return (
     <div className="classes">
-      <div>Classes</div>
+      {AllClasses.map((eachClass) => (
+        <Card key={eachClass.id} className="class_card">
+          <Card.Header className="card_header">
+            {eachClass.className}
+          </Card.Header>
+          <Card.Body className="d-flex card_body">
+            <Card.Text>{eachClass.description}</Card.Text>
+            <Card.Text>
+              <img src={eachClass.image} alt="class" />
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   );
 };
