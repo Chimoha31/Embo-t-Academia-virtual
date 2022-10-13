f4b952030a3f5960d184da9384f6fce3c8805b24
Chimoha31 <mokochii1108@gmail.com>
2022-10-13 15:46:18 -0700

    Changed alert in contact

--- src/components/menubar/MenuBar.jsx
+++ src/components/menubar/MenuBar.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 // import { Link } from "react-router-dom";
 import "./MenuBar.scss";
+import { GiBallerinaShoes } from 'react-icons/gi';
 
 const MenuBar = () => {
   return (
     <div className="menubar">
       <Navbar variant="light" className="navbar">
         <Container>
-          <Navbar.Brand href="/"><i className="fas fa-shoe-prints"></i></Navbar.Brand>
+          <Navbar.Brand href="/"><GiBallerinaShoes /></Navbar.Brand>
+          {/* <Navbar.Brand href="/"><i className="fas fa-shoe-prints"></i></Navbar.Brand> */}
           <Nav className="me-auto">
             <Nav.Link href="/">HOME</Nav.Link>
             <Nav.Link href="classes">CLASSES</Nav.Link>
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { Form, Button, FloatingLabel, Alert } from "react-bootstrap";
 import "./Contact.scss";
+import { MdEmail } from "react-icons/md";
+import { AiFillPhone } from "react-icons/ai";
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -14,9 +16,9 @@ const Contact = () => {
     setSuccess("");
     e.preventDefault();
     if (name === "" || email === "" || comments === "") {
-      setError("Please fill in the blank");
+      setError("Please fill in the all blank");
     } else if (name && email && comments) {
-      setSuccess("Thank you for your message. We will reply within 48 hours.");
+      setSuccess("Thank you for your inquiry.We will reply to you shortly. Please call us if you don't have a response from us within 48 hours. Thank you for your understanding.");
       setName("");
       setEmail("");
       setComments("");
@@ -24,63 +26,86 @@ const Contact = () => {
   };
 
   return (
-    <div className="contact d-flex justify-content-center align-items-center">
-      <Form className="d-flex flex-column form" onSubmit={handleSubmit}>
-        {/* Name */}
-        <Form.Group className="mb-3" controlId="formName">
-          <Form.Label>*Name</Form.Label>
-          <Form.Control
-            type="name"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </Form.Group>
+    <Fragment>
+      {/* Email & phone */}
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <div className="d-flex align-items-center gap-3 mt-5">
+          <AiFillPhone />
+          <p className="mb-0">012-345-6789</p>
+        </div>
+        <div className="d-flex align-items-center gap-3">
+          <MdEmail />
+          <p className="mb-0">emboite.academia@hotmail.com</p>
+        </div>
+      </div>
 
-        {/* Email */}
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>*Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
+      {/* Form */}
+      <div className="contact d-flex justify-content-center align-items-start mt-5">
+        <Form className="d-flex flex-column form" onSubmit={handleSubmit}>
+          {/* Name */}
+          <Form.Group className="mb-3" controlId="formName">
+            <Form.Label>*Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
 
-        {/* Comments */}
-        <Form.Label>*Comments</Form.Label>
-        <FloatingLabel
-          controlId="floatingTextarea2"
-          label="Comments"
-          className="mb-3"
-        >
-          <Form.Control
-            as="textarea"
-            placeholder="Leave a comment here"
-            className="comments"
-            style={{ height: "300px" }}
-            value={comments}
-            onChange={(e) => setComments(e.target.value)}
-          />
-        </FloatingLabel>
+          {/* Email */}
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>*Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
 
-        {error && !success && (
-          <Alert variant="danger" className="mb-2 p-1 text-center msg">
-            {error}
-          </Alert>
-        )}
-        {success && !error && (
-          <Alert variant="success" className="mb-2 p-1 text-center msg">
-            {success}
-          </Alert>
-        )}
+          {/* Comments */}
+          <Form.Label>*Comments</Form.Label>
+          <FloatingLabel
+            controlId="floatingTextarea2"
+            label="Comments"
+            className="mb-3"
+          >
+            <Form.Control
+              as="textarea"
+              placeholder="Leave a comment here"
+              className="comments"
+              style={{ height: "300px" }}
+              value={comments}
+              onChange={(e) => setComments(e.target.value)}
+            />
+          </FloatingLabel>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </div>
+          {error && !success && (
+            <Alert
+              variant="danger"
+              className="mb-3 p-1 text-center msg"
+              style={{fontFamily: "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif"}}
+            >
+              {error}
+            </Alert>
+          )}
+          {success && !error && (
+            <Alert
+              variant="success"
+              className="mb-3 p-1 msg"
+              style={{fontFamily: "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif"}}
+            >
+              {success}
+            </Alert>
+          )}
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </Fragment>
   );
 };
 
